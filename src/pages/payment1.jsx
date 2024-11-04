d4c953a10a3d016fc36195d0119b8846368ecf46
Atmajo <atmajoc@gmail.com>
2024-11-04 23:58:32 +0530

    Add axios dependency and implement payment processing component

--- src/pages/payment1.jsx
+++ src/pages/payment1.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "./App.css";
-import { initializeCashfree } from "./components/cashfree";
+import "../App.css";
+import { initializeCashfree } from "../components/cashfree";
 
 function Payment1() {
   const [cashfreeInstance, setCashfreeInstance] = useState(null);
